ccc1e988fff35062ee22926ffff74869d80ce113
Kellie Watson <kelliewatson@gmail.com>
2017-01-16 15:21:38 -0700

    thought 2nd commit, appears to be the first

---
+++ labs/lab_01/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+* this program is intended to test environment/cofiguration of system.
+**/
+
+
+public class HelloWorld {
+    public static void main (String[] args) {
+        System.out.println("Hello world modification!");
+    }
+}
+
